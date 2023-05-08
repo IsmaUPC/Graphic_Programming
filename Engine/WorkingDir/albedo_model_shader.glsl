8617988f89cb6c0dd9198d5601241816618cc6dc
IsmaUPC <61043099+IsmaUPC@users.noreply.github.com>
2023-05-08 19:40:25 +0200

    PDF3 rendering meshes in progress
    TODO in progress

---
+++ Engine/WorkingDir/albedo_model_shader.glsl
@@ -0,0 +1,55 @@
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+#ifdef TEXTURED_GEOMETRY
+
+#if defined(VERTEX) ///////////////////////////////////////////////////
+
+// TODO: Write your vertex shader here
+layout(location=0) in vec3 aPosition;
+//layout(location = 1) in vec3 aNormal
+layout(location = 2) in vec2 aTexCoord;
+
+//layout(location - 3) in vec3 aTangent;
+//layout(location - 4) in vec3 aBitangent;
+
+out vec2 vTexCoord;
+
+void main()
+{
+	vTexCoord = aTexCoord;
+
+
+	float clippingScale = 5.0;
+
+	gl_Position = vec4(aPosition, clippingScale);
+
+	// Patrick looks away from the camera by default, so i flip it here.
+	gl_Position.z = -gl_Position.z;
+
+}
+//TODO: Seguir desde la slide 3 del pdf 3.Rendering of meshes
+
+#elif defined(FRAGMENT) ///////////////////////////////////////////////
+
+// TODO: Write your fragment shader here
+in vec2 vTexCoord;
+
+uniform sampler2D uTexture;
+
+layout(location=0) out vec4 oColor;
+
+void main()
+{
+	oColor= texture(uTexture,vTexCoord);
+	
+}
+
+#endif
+#endif
+
+
+// NOTE: You can write several shaders in the same file if you want as
+// long as you embrace them within an #ifdef block (as you can see above).
+// The third parameter of the LoadProgram function in engine.cpp allows
+// chosing the shader you want to load by name.
